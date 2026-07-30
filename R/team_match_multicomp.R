2e1ee1853b3f918c53711f2bc7cd4a3febca013e
EuanDewar <39637446+EuanDewar@users.noreply.github.com>
2026-07-30 14:33:45 +0100

    Update team_match_multicomp.R

--- R/team_match_multicomp.R
+++ R/team_match_multicomp.R
@@ -1,4 +1,4 @@
-team_match_multicomp = function (username, password, competitionmatrix, version = "v3", 
+team_match_multicomp = function (username, password, competitionmatrix, version = "v4", 
                                    baseurl = "https://data.statsbomb.com/api/", parallel = TRUE, 
                                    cores = detectCores()) 
 {
